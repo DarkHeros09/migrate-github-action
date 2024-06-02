1c97288b9ffe348fbc897566f8acbe1eb0422b2b
DarkHeros09 <39935182+DarkHeros09@users.noreply.github.com>
2024-06-02 22:08:51 +0200

    fff

--- entrypoint.go
+++ entrypoint.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"bytes"
-	"io"
 	"log"
-	"os"
 )
 
 func main() {
 	var stdBuffer bytes.Buffer
-	mw := io.MultiWriter(os.Stdout, &stdBuffer)
 	log.Println(stdBuffer.String())
 
 }
